7f335a003b016750c515ee311f647ac005f2af96
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-04-21 23:15:24 +0400

    first commit

---
+++ level 1/home/loader.py
@@ -0,0 +1,43 @@
+a = 10
+b = 5
+c = 200
+d = 25
+print( a + b )
+print( a - b )
+print( a * b )
+print( a / b )
+print( a // b)
+print( a ** b)
+print( a % b )
+
+print( c + d )
+print( c - d )
+print( c * d )
+print( c / d )
+print( c // d)
+print( c ** d)
+print( c % d )
+
+print( c + b )
+print( c - b )
+print( c * b )
+print( c / b )
+print( c // b)
+print( c ** b)
+print( c % b )
+
+print( a + d )
+print( a - d )
+print( a * d )
+print( a / d )
+print( a // d)
+print( a ** d)
+print( a % d )
+
+print( b + d )
+print( b - d )
+print( b * d )
+print( b / d )
+print( b // d)
+print( b ** d)
+print( b % d )
