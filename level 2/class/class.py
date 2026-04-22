5f3ecb8f1dfa81e3b68d965c7b9c9a7e22f96655
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-04-23 01:08:44 +0400

    second commit

---
+++ level 2/class/class.py
@@ -0,0 +1,35 @@
+
+
+
+
+age = 200
+print(age)
+
+
+
+midterm = 55
+final = 78
+print(midterm + final)
+
+
+
+logic = 234
+main = 123
+print(logic + main '' final + main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 2/home/lesson2.py
@@ -0,0 +1,113 @@
+
+#ვისწავლეთ
+
+#info
+#Snake case
+#value / მნიშვნელობა
+#Variable / ცლადი
+#Equal / ტოლობა
+
+#რა შემთხვევაში ვიყენებთ Snake case = თუ ჩვენ ცვლად ში გვაქ ერთზე მეტი სიტყვა მაშინ გამოიყენება Snake case მაგალითად hello_world Snake case იმიტო ეწოდება რო _ ძირს ფორთხიალობს 
+
+#Variable / ცვლადი გვეხმარება რო მოვახდინოთ რიცხვებს შორის მათემათიუკური ოპერაცია მაგალითად print(20 + 30) 
+#concatination / კონკატინაცია გამოიყენება სიტყვების გაერთიანებაში  მაგალით x='main' y='last' print(x + '_" + y) Terminal = main_last
+#info =f"" / გვეხმარება ტექსტების ადვილად დაწერაში
+
+#დავალება N3
+
+name = "mate"
+
+age = 14
+
+city = "tbilisi"
+
+temperature = 25
+
+price = 10
+
+d = 6
+g = 12
+
+main = (d + g)
+
+print(main)
+
+print(name, age)
+
+print(name, age, city, temperature, price)
+
+
+name = "mate"
+info = f"hello {name}"
+print(info)
+
+
+info = f"how are you {name}"
+print(info)
+
+country = "georgia"
+city = "tbilisi"
+info = f"where do you live? {country}, {city}"
+print(info)
+
+
+
+
+
+name = "mate"
+age = 14
+country = "georgia"
+city = "tbilisi"
+hobby = "gaming"
+
+info = f"my name is {name}, i am {age} years old, i live in {city}, {country} and my hobby is  {hobby}"
+
+print(info)
+
+
+
+name = "mate"
+last_name = "giorgobiani"
+
+full_name = name + " " + last_name
+
+print(full_name)
+
+
+
+a = 423
+b = 312
+c = 5321
+
+main = a + b
+normal = a - c
+not_last = b * c
+last = a / b
+
+print(main)
+print(normal)
+print(not_last)
+print(last)
+
+d = 21
+h = 542
+j = 213
+z = 23
+print(d + d)
+print(d + h)
+print(d + j)
+print(d + z)
+print(d + z)
+
+print(h + h)
+print(h + z)
+print(h + j)
+print(h + z)
+
+print(j + j)
+print(j + z)
+
+print(z+z)
+
+
+
