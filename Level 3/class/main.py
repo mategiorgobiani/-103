ddc3616d199ad76904968aa3b342e47a8bc87f50
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-04-24 21:28:50 +0400

    second commit

---
+++ Level 3/class/main.py
@@ -0,0 +1,22 @@
+user_age = (input("age ?: "))
+
+
+user_name = (input("name ?: "))
+
+
+user_country = (input("რომელ ქვეყანაში ცხოვრობ ?: "))
+
+user_city = (input("რომელ ქალაქში ცხოვრობ ?: "))
+
+
+print(3 > 453)
+print(532 < 10)
+print(7 == 7)
+print(812 != 2)
+print(6 >= 6)
+print(93 <= 3)
+print(2 > 1)
+print(4 < 232)
+print(144 == 5)
+print(0 != 121)
+
---
+++ Level 3/home/homework.py
@@ -0,0 +1,85 @@
+
+
+
+name = input("შეიყვანე შენი სახელი: ")
+print("გამარჯობა,", name)
+
+
+num = int(input("შეიყვანე რიცხვი: "))
+
+print("ორჯერ მეტი არის:", num + num)
+
+
+a = int(input("პირველი რიცხვი: "))
+b = int(input("მეორე რიცხვი: "))
+print(a + b)
+
+a = int(input())
+b = int(input())
+print(a + b)
+
+print(int(input()) + int(input()))
+print(int(input()) + 5)
+print(10 + int(input()))
+
+
+a = int(input())
+b = int(input())
+print(a - b)
+
+print(int(input()) - int(input()))
+print(int(input()) - 3)
+print(20 - int(input()))
+print(int(input()) - int(input()))
+
+
+a = int(input())
+b = int(input())
+print(a * b)
+
+print(int(input()) * int(input()))
+print(int(input()) * 2)
+print(5 * int(input()))
+print(int(input()) * int(input()))
+
+a = int(input())
+b = int(input())
+print(a / b)
+
+print(int(input()) / int(input()))
+print(int(input()) / 2)
+print(10 / int(input()))
+print(int(input()) / int(input()))
+
+
+name1 = input("1 ადამიანის სახელი: ")
+name2 = input("2 ადამიანის სახელი: ")
+
+text1 = input(name1 + ": ")
+text2 = input(name2 + ": ")
+
+print(name1, "ამბობს:", text1)
+print(name2, "პასუხობს:", text2)
+
+
+# input() არის ფუნქცია, რომელიც გვაძლევს საშუალებას მომხმარებელმა შეიყვანოს ინფორმაცია
+# მაგალითად სახელი, რიცხვი ან ტექსტი
+
+# int() არის ფუნქცია, რომელიც ტექსტს (str) გადააქცევს რიცხვად (int)
+# მაგალითად "5" გახდება 5 და შეგვიძლია მათემატიკური ოპერაციები გავაკეთოთ
+
+# str() არის ფუნქცია, რომელიც რიცხვს ან სხვა ტიპს გადააქცევს ტექსტად
+# მაგალითად 5 გახდება "5" და შეგვიძლია მისი დაბეჭდვა ტექსტთან ერთად
+
+
+
+
+
+
+
+
+
+
+
+
+
