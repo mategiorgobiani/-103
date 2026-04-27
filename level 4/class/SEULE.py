2349ee0e5e1d2f94c9982341e59c8e75e1472e4f
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-04-27 21:15:01 +0400

    second commit

---
+++ level 4/class/SEULE.py
@@ -0,0 +1,41 @@
+
+
+
+#and operator
+
+#True and True =true
+#True and false =false
+#false and true =false
+#false and false =false
+
+#or operator
+
+
+#True and True =true
+#True and false =true
+#false and true =true
+#false and false =false
+
+
+
+wins = int(input())
+ties = int(input())
+
+points = wins * 1 + ties * 0.5
+
+print(points)
+
+
+bill = int(input())
+
+tip = bill * 0.2
+
+print(tip)
+
+
+
+
+
+
+
+
