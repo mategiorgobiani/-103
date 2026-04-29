bebf4e128e036f20fbd84808f27b4a0c12c2f1f8
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-04-29 23:18:54 +0400

    second commit

---
+++ level 5/home/sza.py
@@ -0,0 +1,30 @@
+
+for i in range(1, 21):
+    print(i)
+
+for i in range(2, 51, 2):
+    print(i)
+
+
+n = int(input(" შეიყვანე რიცხვი: "))
+
+total = 0
+
+for i in range(1, n + 1):
+    total = total + i
+
+print("ჯამი :", total)
+
+
+
+main = "ჭამაააა"
+
+for i in main:
+    print(i)
+
+
+
+
+
+
+
