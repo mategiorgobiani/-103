2deff5c98bf3d82a6149fb42f93b26e500b1a4de
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-05-06 01:47:41 +0400

    second commit

---
+++ level 7/class/main.py
@@ -0,0 +1,76 @@
+
+
+
+
+# # Conditional Statments / if else /  პირობითი განცხადებები 
+# # age = int(input('შეიყვანე შერნი წლოვანება:'))
+
+# # if age >= 17:
+
+# #     print('ddd')
+
+# # else:
+# #     print('არ შეგილია მანქანის პრავის აღება')
+
+
+# #თუ არის მომხმარებლის ასაკი არის:
+
+# #elif სხვა შემთხვეწვაში
+
+# age = int(input('შეიყვანე შერნი წლოვანება:'))
+
+# if age >= 2 and age <=10:
+#      print('შენ ხარ უკვე დიდი')
+
+# elif age >= 11 and age <= 17:
+#     print("შენ ხარ თინეიჯერი")
+
+# elif age >= 18 and age <= 30:
+#     print("შენ ხარ სრულწლოვანი")
+
+# elif age >= 31 and age <=49 :
+#     print('შენ ხარ უკვე დიდი')
+
+# elif age >= 50 and age <=70 :
+#     print('შენ უკვე ნახევარი საუკუნე იცოცხლე')
+
+# elif age >= 70 and age <=100:
+#     print('როგორ ხარ პაპიიიი')
+
+# else:
+#     print('გილოცავ,შენ 100 წელზე მეტი იცოცხლე')
+
+
+# num = 8
+# if num % 3 == 0 :
+#     print('Even')
+# else: 
+#     print('Odd')
+
+# for i in range(1 ,15):
+#     if i % 2 == 0:
+#         print(i, 'is Even')
+#     else:
+#         print(i, 'is Odd')
+
+#პროგრამა ამოწმებს რიცხვი თუ არის დადებითი , შემდგომ თუ დადებითი იქნება,
+#მაშინ უნდა შეამოწმოს არის თუ არა რიცხვი ლუწი
+
+# x = 10
+
+# if x > 0: 
+#     print('positive')
+# else:
+#     print('negative')
+
+
+
+num = int(input('enter number:'))
+
+if num > 0:
+    if num % 2 == 0:
+        print('number is positive and even')
+    else:
+        print('Odd')
+else:
+    print('Negative Numver')
---
+++ level 7/home/main.py
@@ -0,0 +1,105 @@
+
+# a = 0
+
+# b = 0
+
+# for i in range(5):
+#     c = int(input("შეიყვანე რიცხვი:"))
+
+#     if c > 0:
+#         a += 1
+
+#     elif c < 0:
+#           b += 1
+
+# print("ნეგატიოურია:", b)
+# print("პოზიტიურია:", a)
+
+
+   
+
+# n = int(input("რიცხვი n: "))
+# num = 0
+
+# for i in range(1, n + 1):
+#     if i % 2 == 0:
+#         num += i
+
+# print("ლუწი რიცხვების ჯამია:", num)
+
+
+# a = int(20)
+# nub = int(input("შეიყვანე რიხცხვი: "))
+# if nub == a:
+#     print("yess you did it")
+
+# else:
+#     print("wrong")
+
+
+
+# a = int(input("შეიყვანე რიცხვი :"))
+
+
+# b = int(input("შეიყვანე რიცხვი :"))
+
+
+# c = int(input("შეიყვანე რიცხვი :"))
+
+
+# num = (a + b + c ) / 3
+
+# print("საშუალო:", num)
+
+# if num > 50:
+#     print("passed")
+
+# else:
+#     print("failed")
+
+
+# for i in range(1, 21):
+#     if i % 3 == 0:
+#         print("Fizz")
+#     else:
+#         print(i)
+
+# num = int(input("შეიყვანე დადებითი რიცხვი:"))
+
+# while num < 0:
+#     print("უარყოფითი რიცხვია შეიყვანე თავიდან:")
+#     num = int(input("შეიყვანე დადებითი რიცხვი: "))
+
+# print(" შენ შეიყვანე დადებითი რიცხვი:")
+
+# a = 0
+
+# b = 0
+
+# for i in range(6):
+#     num = int(input("შეიყვანე რიცხვი!: "))
+
+#     if num % 2 == 0:
+#         a += 1
+#     else:
+#         b += 1
+
+# print("ლუწი რიცხვების რაოდენობა:", a)
+# print("კენტი რიცხვების რაოდენობა:", b)
+
+a = int(input("შეიყვანე რიცხვი: "))
+
+for i in range(4):
+    num = int(input("შეიყვანე რიცხვი: "))
+
+    if num > a:
+        a = num
+
+print("ყველაზე დიდი რიცხვია:", a)
+    
+
+
+
+
+
+
