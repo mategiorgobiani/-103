ac33f5470443f6dea46b85d66feb6a600d7e4568
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-05-11 16:25:44 +0400

    second commit

---
+++ level 9/home/py.py
@@ -0,0 +1,82 @@
+# scores = [45, 67, 92, 88, 34, 99, 76]
+
+# sum_scores = 0
+
+# for score in scores:
+#     if score <= 50:
+#         print(score, "- ჩაიჭრა")
+#     elif score <= 90:
+#         print(score, "- ჩააბარა")
+#     else:
+#         print(score, "- ფრიადი")
+
+#     sum_scores += score
+
+# average = sum_scores / len(scores)
+
+# print("საშუალო არითმეტიკული:", average)
+
+
+
+# numbers = [5, 10, 15, 20, 25]
+
+# sum_numbers = 0
+
+# for num in numbers:
+#     sum_numbers += num
+
+# print("ჯამი არის:", sum_numbers)
+
+
+# numbers = [3, 7, 12, 5, 9, 1, 14, 8, 6, 10]
+
+# if 7 in numbers:
+#     print("რიცხვი 7 არის სიაში")
+# else:
+#     print("რიცხვი 7 არ არის სიაში")
+
+
+# items = [10, "Hello", 3.14, True, "Python", 25]
+
+# count = len(items)
+
+# print("სიაში არის", count, "მნიშვნელობა")
+
+
+# numbers = [12, 45, 7, 89, 23, 56]
+
+# largest = max(numbers)
+
+# print("ყველაზე დიდი რიცხვია:", largest)
+
+
+
+
+
+
+# names = ["Nika", "Luka", "Ana", "Mariam"]
+
+# for name in names:
+#     print("გამარჯობა", name)
+
+
+
+
+# numbers = [1, 2, 3, 2, 4, 2, 5]
+
+# count = 0
+
+# for num in numbers:
+#     if num == 2:
+#         count += 1
+
+# print("რიცხვი 2 მეორდება", count, "-ჯერ")
+
+
+
+# nums = [15, 3, 8, 42, 1]
+
+# smallest = min(nums)
+
+# print("ყველაზე პატარა რიცხვია:", smallest)
+
