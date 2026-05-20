a0624dabe2d4c01186db78005fad12c57b70cb0c
mategiorgobiani <mategiorgobiani622@gmail.com>
2026-05-20 09:24:48 +0400

    second commit

--- level 13/home/seule.py
+++ level 13/home/seule.py
@@ -1,28 +1,123 @@
 
 
-def square(num):
-    return num ** 2
+# # 1
+# def square(number):
+#     return number * number
+
+# print(square(5))
 
 
-def average(a, b, c):
-    return (a + b + c) / 3
+# # 2
+# def average(a, b, c):
+#     return (a + b + c) / 3
+
+# print(average(3, 6, 9))
 
 
-def is_positive(num):
-    return num > 0
+# # 3
+# def is_positive(number):
+#     if number > 0:
+#         return True
+#     else:
+#         return False
+
+# print(is_positive(7))
 
 
-def double(num):
-    return num * 2
+# # 4
+# def double(number):
+#     return number * 2
+
+# print(double(4))
 
 
-def add(a, b=10):
-    return a + b
+# # 5
+# def sum_numbers(a, b=10):
+#     return a + b
+
+# print(sum_numbers(5))
+# print(sum_numbers(5, 3))
 
 
-def greet(name="Guest"):
-    print("Hello,", name)
+# # 6
+# def greet(name="Guest"):
+#     print("Hello", name)
+
+# greet()
+# greet("Nika")
+
+# # 7
+# def sum_numbers():
+#     num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+#     num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+
+#     print("ჯამი არის:", num1 + num2)
+
+# sum_numbers()
 
 
+# # 8
+# def future_age():
+#     age = int(input("შეიყვანე ასაკი: "))
 
-    
+#     print("10 წლის შემდეგ იქნები:", age + 10)
+
+# future_age()
+
+
+# # 9
+# def greet():
+#     name = input("შეიყვანე სახელი: ")
+
+#     print("გამარჯობა,", name)
+
+# greet()
+
+
+# # 10
+# def multiply_numbers():
+#     num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+#     num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+#     num3 = int(input("შეიყვანე მესამე რიცხვი: "))
+
+#     print("ნამრავლი არის:", num1 * num2 * num3)
+
+# multiply_numbers()
+
+
+# # 11
+# def celsius_to_fahrenheit():
+#     celsius = float(input("შეიყვანე ტემპერატურა ცელსიუსში: "))
+
+#     fahrenheit = celsius * 9/5 + 32
+
+#     print("ფარენჰეიტში არის:", fahrenheit)
+
+# celsius_to_fahrenheit()
+
+# # 12
+# def convert_time():
+#     hours = int(input("შეიყვანე საათების რაოდენობა: "))
+
+#     minutes = hours * 60
+#     seconds = hours * 3600
+
+#     print("წუთები:", minutes)
+#     print("წამები:", seconds)
+
+# convert_time()
+
+
+# # 13
+# def average_score():
+#     score1 = int(input("შეიყვანე პირველი ქულა: "))
+#     score2 = int(input("შეიყვანე მეორე ქულა: "))
+#     score3 = int(input("შეიყვანე მესამე ქულა: "))
+#     score4 = int(input("შეიყვანე მეოთხე ქულა: "))
+
+#     average = (score1 + score2 + score3 + score4) / 4
+
+#     print("საშუალო ქულაა:", average)
+
+# average_score()
+
